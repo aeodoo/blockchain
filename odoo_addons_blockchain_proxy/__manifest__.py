3bb5e508b2495ce657db061398c7efa0d1fcda59
Fernando La Chica <fernando.lachica@guadaltech.es>
2021-05-08 15:57:42 +0200

    Including login with Metamask

---
+++ odoo_addons_blockchain_proxy/__manifest__.py
@@ -0,0 +1,24 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Odoo Blockchain proxy",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "summary": "Odoo proxy to AEOdoo Certification",
+    "author": "Dario Lodeiros (CommitSun), "
+    "Eric Antones (NuObit) ,"
+    "Fernando La Chica (Guadaltech), "
+    "Odoo Community Association (OCA)",
+    "website": "https://githut.com/aeodoo/blockhain",
+    "depends": [
+        "blockchain_cert",
+        "event",
+        "survey",
+        "event_sale",
+        "website_event_sale",
+        "auth_metamask",
+    ],
+    "data": [],
+    "application": True,
+    "development_status": "Alpha",
+}
